2a42ccb9efac6319a3a87cf714d0ba0a222e28c7
Justin Proffitt <jprof.dev@gmail.com>
2014-11-15 11:21:39 -0500

    Add functionality that meets user story 1 requirements
    This change also contains tests for user story 1.
    
    User story 1 was unclear as to what would happen once the account numbers were
    parsed, so I went ahead and droppped them into a file that mirrors the input
    filename.
    
    The general approach to parsing the numbers was to look at the input values
    3 characters at a time and map those values to a prime. Since each number is
    composed of 9 total characters, this looks like:
    
     _     2 3 5
    |_| -> 7 11 13
    |_|    17 19 23
    
    When a correct character is encountered int he correct location, we multiply
    that lines value by the corresponding positions prime. Once we have produced
    the three line values for each number, we multiply these values together to
    give us the number we will use to look up the expected value. If we get a hit
    on our product from this process, the input was valid for this number, if we do
    not it was malformed in some way.

--- src/bank_ocr/core.clj
+++ src/bank_ocr/core.clj
@@ -1,7 +1,84 @@
 (ns bank-ocr.core
   (:gen-class))
 
+(def TOP-PRIMES [2  3  5])
+(def MID-PRIMES [7  11 13])
+(def BOT-PRIMES [17 19 23])
+(def NUMBERS {(* 3 7 13 17 19 23)    0
+              (* 13 23)              1
+              (* 3 11 13 17 19)      2
+              (* 3 11 13 19 23)      3
+              (* 7 11 13 23)         4
+              (* 3 7 11 19 23)       5
+              (* 3 7 11 17 19 23)    6
+              (* 3 13 23)            7
+              (* 3 7 11 13 17 19 23) 8
+              (* 3 7 11 13 19 23)    9})
+
+(defn segment-parser
+  "Returns a function that parses a three character segment representing the
+   top, middle, or bottom line of an input value. If an unexpected character
+   is encountered, the returned value will be less than 0.
+   char-values -> values to map to the | or _ if they are present in the
+                  correct location in the character sequence."
+  [char-values]
+  (fn [chrs]
+    (reduce *
+            (map-indexed 
+              (fn [i chr]
+                (cond
+                  (or
+                    (and
+                      (= chr \|)
+                      (or
+                        (= i 0)
+                        (= i 2)))
+                    (and
+                      (= chr \_)
+                      (= i 1))) (nth char-values i)
+                  (= chr \space) 1
+                  :else -1))
+              chrs))))
+
+(def top-parser (segment-parser TOP-PRIMES))
+(def mid-parser (segment-parser MID-PRIMES))
+(def bot-parser (segment-parser BOT-PRIMES))
+
+(defn parse-account-number
+  "Parses the input format into a seq containing the account number. The blankline
+   following the three lines of spaces, |, and _ is expected to be included in the
+   lines arg despite it being ignored."
+  [lines]
+  (map #(NUMBERS %)
+       (apply map *
+              (map (fn [parse-fn line]
+                     (map parse-fn (partition 3 line)))
+                   [top-parser mid-parser bot-parser]
+                   lines))))
+
+(defn parse-file
+  "Read in an input file with account numbers encoded as |, _ and spaces. Parse the
+   numeric values from this input, returning a seq of account numbers of the format:
+   (1 2 3 4 5 6 7 8 9)."
+  [fname]
+  (let [lines (-> (slurp fname) (clojure.string/split #"\n"))]
+    (map parse-account-number (partition 4 lines))))
+
+(defn write-file
+  "Write an output file containing the numeric values of the bank-ocr input format.
+   account-numbers -> output from the parse-file function"
+  [fname account-numbers]
+  (let [str-account-numbers (apply str (map #(str (apply str %) "\n") account-numbers))
+        out-fname (str fname ".out")]
+    (spit out-fname str-account-numbers)))
+
+(defn parse-and-write-file
+  "Takes the path to the input file, processes that file, and outputs the results to
+   $fname.out"
+  [fname]
+  (let [results (parse-file fname)]
+    (write-file fname results)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [filename & args]
+  (parse-and-write-file filename))
--- test/bank_ocr/core_test.clj
+++ test/bank_ocr/core_test.clj
@@ -2,6 +2,45 @@
   (:require [clojure.test :refer :all]
             [bank-ocr.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def all-numbers
+"    _  _     _  _  _  _  _ 
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|")
+
+(def all-numbers-with-fourth-line
+"    _  _     _  _  _  _  _ 
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|
+                           ")
+
+(def all-numbers-top-line-misaligned
+"   _  _     _  _  _  _  _  
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|
+                           ")
+
+(defn split-lines
+  [s]
+  (clojure.string/split s #"\n"))
+
+(deftest good-input
+  (testing "Verify we can parse 123456789, no following empty line."
+    (is (= (parse-account-number (split-lines all-numbers))
+           '(1 2 3 4 5 6 7 8 9)))))
+
+(deftest good-input-following-line
+  (testing "Verify we can parse 123456789, with following empty line."
+    (is (= (parse-account-number (split-lines all-numbers-with-fourth-line))
+           '(1 2 3 4 5 6 7 8 9)))))
+
+(deftest bad-input-top-line-misaligned
+  (testing "Verify we do not parse 123456789 from malformed input"
+    (is (= (parse-account-number (split-lines all-numbers-top-line-misaligned))
+           '(1 nil nil 4 nil nil nil nil nil)))))
+
+(deftest verify-file-parse-and-write
+  (testing "Verify we can read an input file and write the correct results"
+    (let [_ (parse-and-write-file (clojure.java.io/resource "user-story-1-testcases"))
+          expected-results (slurp (clojure.java.io/resource "user-story-1-expected-output"))
+          actual-results (slurp (clojure.java.io/resource "user-story-1-testcases.out"))]
+      (is (= expected-results actual-results)))))
